5840390dd3d6819f105aad221d271fcdb586d005
sawanhooda564172 <sawanhooda9211@gmail.com>
2026-07-18 16:41:33 +0530

    First commit

---
+++ practice.py
@@ -0,0 +1,16 @@
+
+a= 50
+b= 70
+print(a+b)
+length= int(input("enter lenght"))
+breadth= int(input("enter breadth"))
+area= length*breadth
+print(area)
+
+
+radius = float(input("enter r"))
+pie = 22.7
+area = radius*pie**2
+print(area)
+
+
